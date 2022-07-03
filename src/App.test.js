8a08bc51cc4eb4454f8de76e655a57c885faeae4
Kishor Ganeshrao <kishorkg@yahoo.com>
2022-07-03 18:15:25 +1200

    initial commit

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders Lets get started! text', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/Lets get started!/i);
   expect(linkElement).toBeInTheDocument();
 });
